f0fcfc20b51df0bab4b0da983364146afa89b095
aime veidl <aime.verne@live.fr>
2024-05-22 15:27:29 +0200

    category bdd stock composants

--- src/Controller/ComposerController.php
+++ src/Controller/ComposerController.php
@@ -3,52 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Composer;
+use App\Entity\Category;
 use App\Service\ComposerService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class ComposerController extends AbstractController
 {
-    #[Route('/composer', name: 'app_composer')]
-   //   #[Route('/Pcportable')] pour differentes routes pour la meme page
-    public function index(Environment $twig, ComposerService $composerService, ManagerRegistry $doctrine): Response
+    #[Route('/composer/list/{id}', name: 'app_composer')]
+    // #[Route('/Pcportable')] pour différentes routes pour la même page
+    public function index(ComposerService $composerService, ManagerRegistry $doctrine, string $id): Response
     {
-       
-      
-        $composers = $doctrine->getRepository(Composer::class)->findAll();
+        $category = $doctrine->getRepository(Category::class)->find($id);
+        $composers = $doctrine->getRepository(Composer::class)->findBy(['category' => $id]);
 
-        dump($composers);
-        
-        //return new Response($twig->render('composer/index.html.twig', [
-          //'portables' => $portables,
-      //]));
+        if (!$category) {
+            throw $this->createNotFoundException('No category found for id ' . $id);
+        }
 
         return $this->render('composer/index.html.twig', [
+            'category' => $category,
             'composers' => $composers,
         ]);
-  
     }
+
     #[Route('/composer/{id}', name: 'app_composer_show', requirements: ['id' => '\d+'])]
-    public function show( ComposerService $composerService, ManagerRegistry $doctrine, $id): Response
+    public function show(ComposerService $composerService, ManagerRegistry $doctrine, $id): Response
     {
-          //$portable = $portableService->find($id);
-          $composer = $doctrine->getRepository(Composer::class)->find($id);
+        // Utilisez find() au lieu de findBy() pour récupérer un seul objet basé sur l'ID
+        $composer = $doctrine->getRepository(Composer::class)->find($id);
 
-        dump($composer);
+        if (!$composer) {
+            throw $this->createNotFoundException('No composer found for id ' . $id);
+        }
 
-          if (! $composer) {
-              throw $this->createNotFoundException();
-          }
-
-      return $this->render('composer/show.html.twig', [
-       
-        'composer' => $composer,
-
-      ]);
-
-
+        return $this->render('composer/show.html.twig', [
+            'composer' => $composer,
+        ]);
     }
 }
