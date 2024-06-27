99cb9a1da45c66b8fcf880c16a1e0432ecaa15cb
aime veidl <aime.verne@live.fr>
2024-06-27 15:58:39 +0200

    delete profil

--- src/Controller/ComposerController.php
+++ src/Controller/ComposerController.php
@@ -1,38 +1,46 @@
 <?php
-
+/// src/Controller/ComposerController.php
 namespace App\Controller;
 
 use App\Entity\Composer;
 use App\Entity\Category;
-use App\Service\ComposerService;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ComposerController extends AbstractController
 {
     #[Route('/composer/list/{id}', name: 'app_composer')]
-    // #[Route('/Pcportable')] pour différentes routes pour la même page
-    public function index(ComposerService $composerService, ManagerRegistry $doctrine, string $id): Response
+    public function index(ManagerRegistry $doctrine, PaginatorInterface $paginator, Request $request, string $id): Response
     {
         $category = $doctrine->getRepository(Category::class)->find($id);
-        $composers = $doctrine->getRepository(Composer::class)->findBy(['category' => $id]);
 
         if (!$category) {
             throw $this->createNotFoundException('No category found for id ' . $id);
         }
 
+        // Utiliser findBy pour obtenir les résultats
+        $composers = $doctrine->getRepository(Composer::class)->findBy(['category' => $id]);
+
+        // Paginer les résultats
+        $pagination = $paginator->paginate(
+            $composers, /* array of results */
+            $request->query->getInt('page', 1), /* page number */
+            2 /* limit per page */
+        );
+
         return $this->render('composer/index.html.twig', [
             'category' => $category,
-            'composers' => $composers,
+            'pagination' => $pagination,
         ]);
     }
 
     #[Route('/composer/{id}', name: 'app_composer_show', requirements: ['id' => '\d+'])]
-    public function show(ComposerService $composerService, ManagerRegistry $doctrine, $id): Response
+    public function show(ManagerRegistry $doctrine, string $id): Response
     {
-        // Utilisez find() au lieu de findBy() pour récupérer un seul objet basé sur l'ID
         $composer = $doctrine->getRepository(Composer::class)->find($id);
 
         if (!$composer) {
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -1,15 +1,25 @@
 <?php
-
+// src/Controller/ProfileController.php
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfileType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
+    private $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route("/profile", name:"app_profil")]
     public function index(UserRepository $userRepository): Response
     {
@@ -25,7 +35,52 @@ class ProfileController extends AbstractController
             'users' => $users,
         ]);
     }
+
+    #[Route("/profile/edit", name:"app_profil_edit")]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException('No user found');
+        }
+
+        $form = $this->createForm(ProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Profile updated successfully!');
+
+            return $this->redirectToRoute('app_profil');
+        }
+
+        return $this->render('profil/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    #[Route("/profil/delete", name:"app_profil_delete")]
+    public function delete(): RedirectResponse
+    {
+        // Récupérer l'utilisateur actuellement connecté
+        $user = $this->getUser();
+
+        // Vérifier si l'utilisateur est connecté
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé');
+        }
+
+        // Supprimer l'utilisateur
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+        session_destroy();
+
+        // Redirection vers une page de confirmation ou une autre page
+        return $this->redirectToRoute('app_home'); // Redirige vers la page d'accueil par exemple
+    }
 }
-
-
 
